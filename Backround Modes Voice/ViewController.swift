e322a959e4437390c96fa87ab85a3be39b62e21b
brtyvz <beratttyavuzz@gmail.com>
2022-02-20 01:12:11 +0300

    son

--- Backround Modes Voice/ViewController.swift
+++ Backround Modes Voice/ViewController.swift
@@ -6,14 +6,29 @@
 //
 
 import UIKit
-
+import AVFoundation
 class ViewController: UIViewController {
-
+var sesOynatici = AVAudioPlayer()
     override func viewDidLoad() {
+        do{
+            let dosyaYolu = Bundle.main.path(forResource: "music", ofType:".mp3")
+            
+            sesOynatici = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: dosyaYolu!))
+            sesOynatici.prepareToPlay()
+        }catch{
+            
+            print(error.localizedDescription)
+        }
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func basla(_ sender: Any) {
+        sesOynatici.play()
+    }
+    
+    @IBAction func dur(_ sender: Any) {
+        sesOynatici.stop()
+    }
 }
 
